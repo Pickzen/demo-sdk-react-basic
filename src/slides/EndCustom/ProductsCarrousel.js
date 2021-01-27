a793db47d1aafbea55a428e3e632f1f589e0aba9
MrR3set <bairon.pazrobles@gmail.com>
2021-01-27 10:40:46 -0800

    Created products carrousel

---
+++ src/slides/EndCustom/ProductsCarrousel.js
@@ -0,0 +1,58 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { SlideContext } from '../../context/SlideContext';
+import Result from '../../components/Result/Result'
+import './EndCustom.css'
+
+
+function ProductsCarrousel({results,className=''}) {
+    const { slideModel } = useContext(SlideContext);
+
+    const target = React.createRef();
+    
+    const [isPhone,setIsPhone] = useState(true);
+    const [scrollProgress, setScrollProgress] = useState(0);
+    const [scrollSize, setScrollSize] = useState(results.length)
+
+
+    useEffect(()=>{
+        setIsPhone(window.innerWidth<600?true:false);
+        
+        setScrollSize(isPhone?results.length:Math.ceil(results.length/3))
+
+        target.current.addEventListener('touchmove', scrollListener);
+        return () => target.current && target.current.removeEventListener('touchmove', scrollListener);
+    })
+
+    const scrollListener = () => {
+        if(!target.current)
+            return;
+        
+        const element = target.current;
+        const windowScroll = element.scrollLeft;
+        const totalWidth = element.scrollWidth - element.clientWidth;
+
+        if (windowScroll === 0)
+            return setScrollProgress(0);
+        
+        if(windowScroll > totalWidth)
+            return setScrollProgress(100);
+        
+        setScrollProgress( (windowScroll/totalWidth) * 100);
+    }
+
+    return (
+        <div className={`pz-products ` + className}> 
+            <div className='productsView' ref={target}>
+                {results.map((result,i) =>  <Result key={result.getId()} value={result}/>)}
+            </div>
+
+            <div className='controls'>
+                {[...Array(Math.ceil(scrollSize)).keys()].map(i=>
+                <div className={`step ${((scrollProgress * scrollSize) / 100 >= i && (scrollProgress * scrollSize) / 100 <= i + 1)?'active':''}`} />)}
+            </div>
+        </div>
+    );
+
+}
+
+export default ProductsCarrousel;
