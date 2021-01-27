35b893ba924d11dcc43cb1c702d026bba746e343
MrR3set <bairon.pazrobles@gmail.com>
2021-01-27 10:40:38 -0800

    Created Custom end slide

---
+++ src/slides/EndCustom/EndCustom.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { SlideContext } from '../../context/SlideContext';
+import Result from '../../components/Result/Result'
+import Header from "../../components/Header/Header";
+import './EndCustom.css'
+import Navigation from "../../components/Navigation/Navigation";
+import ProductsCarrousel from './ProductsCarrousel';
+
+function EndCustom() {
+    const { slideModel } = useContext(SlideContext);
+    const [results, setResults] = useState(null);
+
+
+    useEffect(()=>{
+        if(!results){
+            slideModel.getResults().then( (results) => {
+                setResults(results);
+            }).catch( (reason) => {
+                console.error(reason);
+            })
+        }
+    })
+    
+    return (
+        <div className="slide end">
+            {results?(
+                <>
+                    <Header />
+                    <div className="results">
+                    {/* Add logic to determinate which products go where */}
+                        <ProductsCarrousel results={results} className='pz-topView'/>
+                        <ProductsCarrousel results={results} className='pz-bottomView'/>
+                        
+                    </div>    
+                    <Navigation />
+                </>
+            ):(
+                <div className="loading">Loading ...</div>
+            )}
+        </div>
+    );
+}
+
+export default EndCustom;
