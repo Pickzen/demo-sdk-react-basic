d925dc8f2536cba503d8b9973ca446d262579f0c
MrR3set <bairon.pazrobles@gmail.com>
2021-01-27 10:40:23 -0800

    Changed result to display properly

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -5,8 +5,10 @@ import './Result.css'
 const Result = ({value:result}) => {
     return (
 	    <div className="result">
-			<div className="image" style={{backgroundImage:`url(${result.getImage()})`}}></div>
 	        <CustomHTML html={result.getTitle()} />
+			<div className="image" style={{backgroundImage:`url(${result.getImage()})`}}></div>
+	        <CustomHTML html={"$"+result.getPrice()}/>
+			
 	    </div>
     )
 };
