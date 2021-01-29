1a4ef9554f16f8e9f082394080067e4c4f0f46a2
MrR3set <bairon.pazrobles@gmail.com>
2021-01-28 20:47:46 -0800

    Added scroll to item when clicking dots

--- src/slides/EndCustom/ProductsCarrousel.js
+++ src/slides/EndCustom/ProductsCarrousel.js
@@ -5,7 +5,6 @@ import './EndCustom.css';
 
 function ProductsCarrousel({results,className='',title}) {
     const { slideModel } = useContext(SlideContext);
-
     const target = React.createRef();
     
     const [isPhone,setIsPhone] = useState(true);
@@ -29,14 +28,25 @@ function ProductsCarrousel({results,className='',title}) {
         const element = target.current;
         const windowScroll = element.scrollLeft;
         const totalWidth = element.scrollWidth - element.clientWidth;
-
+        
         if (windowScroll === 0)
             return setScrollProgress(0);
         
         if(windowScroll > totalWidth)
             return setScrollProgress(100);
-        
+
         setScrollProgress( (windowScroll/totalWidth) * 100);
+    }
+    
+    const scrollToPage = (i) => {
+        const element = target.current;
+
+        const firstChild = element.children[0]; 
+        // Calculates the final width of each 'page'
+        const pageWidth = ((firstChild.offsetWidth + parseInt(getComputedStyle(firstChild).margin) * 2) * (results.length/scrollSize));
+        element.scrollLeft = pageWidth * (i)
+        
+        setScrollProgress( (element.scrollLeft/element.scrollWidth - element.clientWidth) * 100);
     }
 
     return (
@@ -48,7 +58,8 @@ function ProductsCarrousel({results,className='',title}) {
 
             <div className='pz-scrollProgress'>
                 {[...Array(Math.ceil(scrollSize)).keys()].map(i=>
-                <div className={`pz-step ${((scrollProgress * scrollSize) / 100 >= i && (scrollProgress * scrollSize) / 100 <= i + 1)?'active':''}`} />)}
+                <div onClick={() => scrollToPage(i)}
+                className={`pz-step ${((scrollProgress * scrollSize) / 100 >= i && (scrollProgress * scrollSize) / 100 <= i + 1)?'active':''}`} />)}
             </div>
         </div>
     );
