1a9703a9e1ad6078358699a22f95bbdcbde16a0b
Isidro <isidro@pickzen.com>
2021-06-09 17:00:38 +0200

    Fix end slide

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import CustomHTML from "../CustomHTML/CustomHTML";
 import './Result.css'
 
-const Result = ({value:result}) => {
-	const [id, setId] = useState(result.getId());
-	const [image, setImage] = useState(result.getImage(true));
-	const [title, setTitle] = useState(result.getTitle());
-	const [price, setPrice] = useState(result.getLocalizedPrice());
+const Result = ({value}) => {
+	const [result, setResult] = useState(value);
+
+	useEffect( () => {
+		if (result.hasVariants()) {
+			// Preselects the first variant
+			let firstVariantId = result.getFirstVariant().getId();
+			selectVariantHandler(firstVariantId);
+		}
+	}, []);
 
 	const selectVariantHandler = (variantId) => {
-		const variant = result.getVariant(variantId);
-		setId(variant.getId());
-		setImage(variant.getImage(true));
-		setPrice(variant.getLocalizedPrice());
+		let updatedResult = result.setVariant(variantId);
+		setResult(updatedResult);
 	}
 
 	const selectResultHandler = (id) => {
@@ -33,15 +36,15 @@ const Result = ({value:result}) => {
 
 	return (
 		<div className="result">
-			<div className="image" style={{backgroundImage:`url(${image})`}}></div>
+			<div className="image" style={{backgroundImage:`url(${result.getImage()})`}}></div>
 
-			<CustomHTML html={title} />
+			<CustomHTML html={result.getTitle()} />
 
-			<div className="price">{price}</div>
+			<div className="price">{result.getLocalizedPrice()}</div>
 
 			{result.hasVariants()?getVariantsSelector():null}
 
-			<button onClick={ (e) => selectResultHandler(id)}>Select</button>
+			<button onClick={ (e) => selectResultHandler(result.getId())}>Select</button>
 		</div>
 	)
 };
